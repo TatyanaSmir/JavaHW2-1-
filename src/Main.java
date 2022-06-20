bff99830bebad76dba0a1f7b26f24e29fd3d0915
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-06-20 18:41:17 +0300

    Mile

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       // int cost = 500;
+       // int mile = cost / 20;
+       // System.out.println(" Количество бонусных миль:" + mile)
+        int cost = 500;
+        int x = 20;
+        int mile = cost / x;
+        System.out.println(" количество бонусных миль: "+ mile);
+    }
+}
